fc4913f72afdd49358fe31c6842dea26e9f8e952
Your Name <you@example.com>
2018-05-11 21:18:51 +0400

    aa

--- public/script.js
+++ public/script.js
@@ -1,50 +1,12 @@
-function genMatrix(w, h) {
-    var matrix = [];
-    for (var y = 0; y < h; y++) {
-        matrix[y] = [];
-        for (var x = 0; x < w; x++) {
-            var r = random(120);
-            if (r < 20) r = 0;
-            else if (r < 65) r = 1;
-            else if (r < 90) r = 2;
-            else if (r < 100) r = 3;
-            else if (r < 120) r = 4;
-            matrix[y][x] = r;
-        }
-    }
-    return matrix;
-}
-
-var matrix;
-var w = 30;
-var h = 30;
-var side = 24;
-var grassArr = [], xotakerArr = [], gishatichArr = [], amenakerArr = [];
+var socket = io();
 
 function setup() {
-    matrix = genMatrix(w, h);
-    createCanvas(side * w, side * h);
+    createCanvas(800, 800);
     background("#acacac");
     frameRate(5);
-    for (var y in matrix) {
-        for (var x in matrix[y]) {
-            if (matrix[y][x] == 1) {
-                grassArr.push(new Grass(x * 1, y * 1, 1));
-            }
-            else if (matrix[y][x] == 2) {
-                xotakerArr.push(new Xotaker(x * 1, y * 1, 2));
-            }
-            else if (matrix[y][x] == 3) {
-                gishatichArr.push(new Gishatich(x * 1, y * 1, 3))
-            }
-            else if (matrix[y][x] == 4) {
-                amenakerArr.push(new Amenaker(x * 1, y * 1, 4))
-            }
-        }
-    }
 }
 
-function draw() {
+function genMatrix(matrix) {
     background("#acacac");
     for (var y in matrix) {
         for (var x in matrix[y]) {
@@ -67,27 +29,10 @@ function draw() {
         }
     }
 
-    for (var i in grassArr) {
-        grassArr[i].mul();
-    }
+    // console.log(matrix);
 
-    for (var i in xotakerArr) {
-        xotakerArr[i].bazmanal();
-        xotakerArr[i].utel();
-        xotakerArr[i].mahanal();
-    }
-
-    for (var i in gishatichArr) {
-        gishatichArr[i].bazmanal();
-        gishatichArr[i].utel();
-        gishatichArr[i].mahanal();
-    }
-    for (var i in amenakerArr) {
-        amenakerArr[i].utel();
-        amenakerArr[i].sharjvel();
-        amenakerArr[i].mahanal();
-    }
-
-   // console.log(matrix);
+    socket.emit("send matrix", matrix);
 
 }
+socket.on("gen matrix", genMatrix);
+ 
